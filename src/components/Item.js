b1d064806d95b2cbea348ed7d60d62a2f6779fec
anissalopez <anissa965@gmail.com>
2023-06-29 12:35:35 -0400

    learning state

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  const [cart, setCart] = useState("")
+  let text = cart === "" ? `Add to Cart` : `Remove from Cart`
+  let buttonClass = cart === "" ? "add" : "remove"
+
+  function addToCart(){
+  
+   return(setCart((cart) => cart === "" ?  `in-cart` : "")) 
+
+  }
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={addToCart}>{text}</button>
     </li>
   );
 }
